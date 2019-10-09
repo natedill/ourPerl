c1487ab909bf34315330c31cfff782c675b42e31
natedill <natedill@gmail.com>
2019-10-09 17:26:28 -0400

    change to pointTree2pointsKMZ.pl

--- Lidar/PointTree2pointsKMZ.pl
+++ Lidar/PointTree2pointsKMZ.pl
@@ -9,18 +9,19 @@ use Lidar::PointTree;
 my $treeFile='tree.tree2';
 my $colorFile='c:/ourPerl/jet.txt';
 
-my @CLIM=(7,13);
-my $numColors=12;
+my @CLIM=(-6,10);
+my $numColors=10;
 my $addAdjust=0;
 my $multAdjust=3.280833333;
 my $pngDir='pImages';
 my $kmlDir='pFiles';
 my $cbarTitle='Elevation NAVD88 Feet';
-my $nskip=2;
+my $nskip=50;
+my $iconScale=0.5;
+my $labelScale=0.5;
 
 
 my $tree=PointTree->loadTree($treeFile); 
-
 
 
 $tree->writeKMLPoints(
@@ -32,9 +33,11 @@ $tree->writeKMLPoints(
                         -ZADDADJUST=>$addAdjust,
                         -ZMULTADJUST=>$multAdjust,
                         -PNGDIR=>$pngDir,
+                        -NNSKIP=>$nskip, # use every nskip points
                         -CBARTITLE=>$cbarTitle, 
-                        -NSKIP=>$nskip 
+                        -ICONLABELSCALE=>[$iconScale, $labelScale]
                         );
+$tree->makeColorbar($cbarTitle);
 
 
 
